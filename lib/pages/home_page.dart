b482e23dbb3d601e5bfb855776c158761361c2d5
hashimo000 <hashimon.hashimon@icloud.com>
2023-10-02 00:18:05 -0700

    ホームのUI

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -4,6 +4,7 @@ import 'package:famous_quote_app/main.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:intl/intl.dart';
 
 
 
@@ -33,31 +34,51 @@ class _MyHomePageState extends State<MyHomePage> {
         title: Text(widget.title),
       ),
       body: Center(
-        // child:const CustomScrollView(
-        //   slivers: [
-        //     CupertinoSliverNavigationBar(
-        //       backgroundColor: Colors.black,
-        //       largeTitle: Text("通知時間", style:TextStyle(color: Colors.white)),
-        //       trailing: GestureDetector(
-        //         child:Icon(Icons.add,color: Colors.orange)),
-        //     )
-        //   ],
-        // ),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            ElevatedButton(
-               onPressed: () {
-                notification();  // ここでnotification関数を呼び出します
-    ScaffoldMessenger.of(context).showSnackBar(
-      const SnackBar(content: Text("ボタンを押下しました")),
-    );  // また、SnackBarを表示してボタンが押されたことをユーザーにフィードバックするように追加します
-               },
-               child: const Text('+1 いいね!'),
-               
+        child: CustomScrollView(
+          slivers: [
+            CupertinoSliverNavigationBar(
+              backgroundColor: Colors.black,
+              largeTitle:const Text("通知時間", style:TextStyle(color: Colors.white)),
+              trailing: GestureDetector(
+                child:const Icon(Icons.add,color: Colors.orange),
+                onTap: (){
+
+                },
+                ),
+                
             ),
+            SliverList(
+              delegate: SliverChildBuilderDelegate(
+                (context, index) {
+                  Alarm alarm =alarmList[index];
+                  return Column(
+                    children: [
+                      ListTile(
+                        title: Text(DateFormat("H,mm").format(alarm.alarmTime)),
+                      )
+                    ],
+                  );
+                },
+                childCount: alarmList.length
+                ),
+                )
           ],
         ),
+    //     child: Column(
+    //       mainAxisAlignment: MainAxisAlignment.center,
+    //       children: [
+    //         ElevatedButton(
+    //            onPressed: () {
+    //             notification();  // ここでnotification関数を呼び出します
+    // ScaffoldMessenger.of(context).showSnackBar(
+    //   const SnackBar(content: Text("ボタンを押下しました")),
+    // );  // また、SnackBarを表示してボタンが押されたことをユーザーにフィードバックするように追加します
+    //            },
+    //            child: const Text('+1 いいね!'),
+               
+    //         ),
+    //       ],
+    //     ),
       ),
     );
   }
