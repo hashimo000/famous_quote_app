589db38e99e84eb12cd1aeafa26b3842d592f287
hashimo000 <hashimon.hashimon@icloud.com>
2023-10-01 19:30:48 -0700

    アラームファイルの作成

---
+++ lib/alarm.dart
@@ -0,0 +1,7 @@
+class Alarm{
+  int id;
+  DateTime alarmTime;
+  bool isActive;
+
+  Alarm ({this.id = 0,required this.alarmTime,this.isActive=true});
+}
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,5 +1,7 @@
 
+import 'package:famous_quote_app/alarm.dart';
 import 'package:famous_quote_app/main.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 
@@ -18,7 +20,12 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  
+  List<Alarm> alarmList =[
+    Alarm(alarmTime:DateTime.now()),
+  ];
+    
+
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -26,6 +33,16 @@ class _MyHomePageState extends State<MyHomePage> {
         title: Text(widget.title),
       ),
       body: Center(
+        // child:const CustomScrollView(
+        //   slivers: [
+        //     CupertinoSliverNavigationBar(
+        //       backgroundColor: Colors.black,
+        //       largeTitle: Text("通知時間", style:TextStyle(color: Colors.white)),
+        //       trailing: GestureDetector(
+        //         child:Icon(Icons.add,color: Colors.orange)),
+        //     )
+        //   ],
+        // ),
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
