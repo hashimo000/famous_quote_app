8f05a2ee41be2ac553b816edacfdefee1cc3b329
hashimo000 <hashimon.hashimon@icloud.com>
2023-10-04 05:35:27 -0700

    sqflite

--- lib/sqflite.dart
+++ lib/sqflite.dart
@@ -1,11 +1,24 @@
 import 'package:sqflite/sqflite.dart';
-
+import 'package:path/path.dart';
 class DbProvider{
   static Database? database;
   static final String tableName ="alarm";
 
   static Future<void>_createTable(Database db ,int version)async{
 
- await db.execute("CREATE TABLE $tableName(id INTEGER PRIMARY KEY AUTOINCREMENT ,alarm_time TEXT,is_active INTEGER)");
+ await db.execute("CREATE TABLE $tableName(id INTEGER PRIMARY KEY AUTOINCREMENT ,alarm_time TEXT,isactive INTENGER)");
+}
+static Future<Database> initDb() async{
+  String path = join(await getDatabasesPath(),"alarm_app.db");
+  return await openDatabase(path, version:1,onCreate:_createTable);
+}
+
+static Future<Database?> setDb()async{
+  if(database == null){
+    database= await initDb();
+    return database;
+  }else{
+    return database;
+  }
 }
 }
