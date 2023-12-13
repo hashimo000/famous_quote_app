d3a10751480a3352461cb3f346f85fda3caf9c63
hashimo000 <hashimon.hashimon@icloud.com>
2023-12-14 04:26:01 +0900

    appbar

--- lib/pages/others_page.dart
+++ lib/pages/others_page.dart
@@ -1,12 +1,17 @@
 import 'package:flutter/material.dart';
 
+
 class OthersPage extends StatelessWidget {
   const OthersPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-       debugShowCheckedModeBanner: false,
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('使用方法'),
+      ),
+
+      
     );
   }
 }
