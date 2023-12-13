72836deb801472ef7bc7b3de10ffd6450a740438
hashimo000 <hashimon.hashimon@icloud.com>
2023-12-13 20:40:19 +0900

    debugShowCheckedBanner false

--- lib/pages/others_page.dart
+++ lib/pages/others_page.dart
@@ -5,6 +5,8 @@ class OthersPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return const MaterialApp(
+       debugShowCheckedModeBanner: false,
+    );
   }
 }
