6242f7bf9eef14c4990cd2af79303f9424602a0a
hashimo000 <hashimon.hashimon@icloud.com>
2023-12-22 16:45:39 +0900

    privace suportpage create

--- lib/pages/others_page.dart
+++ lib/pages/others_page.dart
@@ -3,6 +3,7 @@ import 'package:url_launcher/url_launcher.dart';
 
 class OthersPage extends StatelessWidget {
   final Uri url = Uri.parse('https://forms.gle/QyrNd6jFdikaPaLb7'); 
+  final Uri url2 = Uri.parse('https://www.termsfeed.com/live/f268ae8b-64b3-4246-90f4-e2f644dd9cb5'); 
 
   OthersPage({super.key});
 
@@ -17,19 +18,28 @@ class OthersPage extends StatelessWidget {
           Expanded(  
             child: ListView(
               children: <Widget>[
-                 Padding(
+                Padding(
                   padding: const EdgeInsets.only(top: 51.0),
-                  child:Center(
-                  child: ElevatedButton(
-                    onPressed: () => _launchUrl(url), 
-                    child: const Text('お問い合わせ'),
+                  child: Center(
+                    child: Column(
+                      mainAxisSize: MainAxisSize.min, 
+                      children: <Widget>[
+                        ElevatedButton(
+                          onPressed: () => _launchUrl(url), 
+                          child: const Text('お問い合わせ'),
+                        ),
+                        const SizedBox(height: 20),
+                        ElevatedButton(
+                          onPressed: () => _launchUrl(url2), 
+                          child: const Text('プライバシーサポート'),
+                        ),
+                      ],
+                    ),
                   ),
                 ),
-                 )
               ],
             ),
           ),
-      
         ],
       ),
     );
